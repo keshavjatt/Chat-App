99b5ace09cbd76ff37c6a277416e55d18782f54e
keshavjatt <thecoder780@gmail.com>
2025-01-11 21:46:53 +0530

    Created Register page on FE side

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div className='mt-5'>
+        <div className='bg-white '>
+            <h3>Welcome to Chat app!</h3>
+        </div>
+    </div>
+  )
+}
+
+export default RegisterPage
