cf117a0fde52a43cd52677159dc94c9f3652e2e4
keshavjatt <thecoder780@gmail.com>
2025-01-12 22:01:11 +0530

    Designing Regitser page on FE Side

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { IoClose } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
   const [data, setData] = useState({
@@ -7,6 +9,8 @@ const RegisterPage = () => {
     password : "",
     profile_pic : "",
   })
+  const [uploadPhoto, setUploadPhoto] = useState("")
+
 
   const handleOnChange = (e) => {
     const { name, value } = e.target
@@ -19,12 +23,29 @@ const RegisterPage = () => {
     })
   }
 
+  const handleUploadPhoto = (e) => {
+    const file = e.target.files[0]
+
+    setUploadPhoto(file)
+  }
+  const handleClearUploadPhoto = (e) => {
+    e.stopPropagation()
+    e.preventDefault()
+    setUploadPhoto(null)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    console.log(data, "Regiter Data:")
+  }
+
   return (
     <div className='mt-5'>
-        <div className='bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4'>
+        <div className='bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4 mx-auto'>
             <h3>Welcome to Chat app!</h3>
 
-            <form className='grid gap-4 mt-5'>
+            <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-1'>
                     <label htmlFor='name'>Name :</label>
                     <input
@@ -68,17 +89,43 @@ const RegisterPage = () => {
                 </div>
 
                 <div className='flex flex-col gap-1'>
-                    <label htmlFor='profile_pic'>Photo :</label>
-                    {/* <input
+                    <label htmlFor='profile_pic'>Photo :
+
+                      <div className='h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer'>
+                          <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1'>
+                            {
+                              uploadPhoto?.name ? uploadPhoto?.name : "Upload profile photo"
+                            }
+                          </p>
+                          {
+                            uploadPhoto?.name && (
+                              <button className='text-lg ml-2 hover:text-red-600' onClick={handleClearUploadPhoto}>
+                                <IoClose />
+                              </button>
+                            )
+                          }
+                      </div>
+
+                    </label>
+
+                    <input
                       type='file'
                       id='profile_pic'
                       name='profile_pic'
-                      className='bg-slate-100 px-2 py-1 focus:outline-primary'
-                    />  */}
+                      className='bg-slate-100 px-2 py-1 focus:outline-primary hidden'
+                      onChange={handleUploadPhoto}
+                    /> 
                 </div>
 
+                <button 
+                  className='bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide'
+                >
+                  Register
+                </button>
 
             </form>
+
+            <p className='my-3 text-center'>Already have account ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
         </div>
     </div>
   )
