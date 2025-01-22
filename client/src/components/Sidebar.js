acb58a5cc4759dc7193ed210dfb052d4563057f3
keshavjatt <thecoder780@gmail.com>
2025-01-22 22:33:53 +0530

    add user and logout icon in Sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import { IoChatbubbleEllipses } from "react-icons/io5"
+import { FaUserPlus } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { BiLogOut } from "react-icons/bi"
+import Avatar from "./Avatar";
+import { useSelector } from 'react-redux'
 
 const Sidebar = () => {
+  const user = useSelector(state => state.user)
   return (
     <div className='w-full h-full'>
-        <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5'>
-            <div className='w-12 h-12 bg-red-500 flex justify-center items-center' title='Chat'>
-                <IoChatbubbleEllipses 
-                    size={25}                
-                />
+        <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
+            <div>
+                <NavLink className={({isActive})=>`w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && "bg-slate-200"}`} title='Chat'>
+                    <IoChatbubbleEllipses 
+                        size={20}                
+                    />
+                </NavLink>
+
+                <div className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded' title='add friend'>
+                    <FaUserPlus 
+                        size={20}
+                    />
+                </div>
+            </div>
+
+            <div className='flex flex-col items-center'>
+                <button className='mx-auto'>
+                    <Avatar 
+                      width={40}
+                      height={40}
+                      // name={}
+                    />
+                </button>
+                <button className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded' title='logout'>
+                    <span className='-ml-2'>
+                        <BiLogOut 
+                            size={20}
+                        />
+                    </span>
+                </button>
             </div>
         </div>
     </div>
