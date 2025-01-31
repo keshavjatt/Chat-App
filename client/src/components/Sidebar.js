5b8374ff52b8bc64f86d47323c0419a7571acc4c
keshavjatt <thecoder780@gmail.com>
2025-02-01 00:08:13 +0530

    worked on sidebar in FE side

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,12 +6,13 @@ import { BiLogOut } from "react-icons/bi"
 import Avatar from "./Avatar";
 import { useSelector } from 'react-redux'
 import EditUserDetails from './EditUserDetails';
+import Divider from './Divider';
 
 const Sidebar = () => {
   const user = useSelector(state => state.user)
   const [editUserOpen, seteditUserOpen] = useState(false)
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
         <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
             <div>
                 <NavLink className={({isActive})=>`w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && "bg-slate-200"}`} title='Chat'>
@@ -46,6 +47,16 @@ const Sidebar = () => {
             </div>
         </div>
 
+        <div className='w-full'>
+            <div className='h-16 flex items-center'>
+                <h2 className='text-xl font-bold p-4 text-slat-800'>Message</h2>
+            </div>
+            <div className='bg-slate-200 p-[0.5px]'></div>
+                
+            <div className='bg-red-500 h-[calc(100vh-65px)]'>
+
+            </div>
+        </div>
 
         {/** Edit User Details **/}
         {
