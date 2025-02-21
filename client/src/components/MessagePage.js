a04adc8590acddcd2c0a6fcc773f6772bf05f583
keshavjatt <thecoder780@gmail.com>
2025-02-21 23:44:03 +0530

    worked on Image and Vidoe Toggle in FE side

--- client/src/components/MessagePage.js
+++ client/src/components/MessagePage.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Avatar from "./Avatar";
 import { HiDotsVertical } from "react-icons/hi";
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaPlus } from 'react-icons/fa6';
+import { FaImage } from 'react-icons/fa6';
+import { FaVideo } from 'react-icons/fa6';
 
 const MessagePage = () => {
   const params = useParams();
@@ -33,6 +37,10 @@ const MessagePage = () => {
       <header className='sticky top-0 h-16 bg-white flex items-center gap-4 px-4 justify-start'>
         {/* Left Side */}
         <div className="flex items-center gap-4">
+          <Link to={"/"} className='lg:hidden'>
+              <FaAngleLeft size={25} />
+          </Link>
+
           <Avatar
             width={50}
             height={50}
@@ -41,7 +49,7 @@ const MessagePage = () => {
             userId={dataUser?._id}
           />
           <div className='-mt-2'>
-            <h3 className='font-semibold text-lg my-0'>{dataUser?.name}</h3>
+            <h3 className='font-semibold text-lg my-0 text-ellipsis line-clamp-1'>{dataUser?.name}</h3>
             <p className='-my-2 text-sm'>
               {dataUser.online ? <span className='text-primary'>online</span> : <span className='text-slate-400'>offline</span>}
             </p>
@@ -55,6 +63,39 @@ const MessagePage = () => {
             </button>
         </div>
       </header>
+
+      {/** Show all message  **/}
+      <section className='h-[calc(100vh-128px)] overflow-x-hidden overflow-y-scroll scrollbar'>
+        Show all message
+      </section>
+
+      {/** Send message **/}
+      <section className='h-16 bg-white flex items-center px-4'>
+        <div className='relative'>
+            <button className='flex justify-center items-center w-11 h-11 rounded-full hover:bg-primary hover:text-white'>
+                <FaPlus size={20} />
+            </button>
+
+            {/** Video and Image **/}
+            <div className='bg-white shadow rounded absolute bottom-14 w-36 p-2'>
+                <form>
+                    <label htmlFor='uploadImage' className='flex items-center p-2 px-3 gap-3 hover:bg-slate-200 cursor-pointer'>
+                        <div className='text-primary'>
+                            <FaImage size={18} />
+                        </div>
+                        <p>Image</p>
+                    </label>
+
+                    <label htmlFor='uploadVideo' className='flex items-center p-2 px-3 gap-3 hover:bg-slate-200 cursor-pointer'>
+                        <div className='text-purple-500'>
+                            <FaVideo size={18} />
+                        </div>
+                        <p>Video</p>
+                    </label>
+                </form>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
